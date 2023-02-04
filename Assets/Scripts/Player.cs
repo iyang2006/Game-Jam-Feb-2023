a255b334a55f75a3c12f6450b2f1c7e0b61cb41b
theNareynater <whitacre@purdue.edu>
2023-02-04 15:29:10 -0500

    Better Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,30 +7,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public int WaterPoints
-    {
-        get { return WaterPoints; }
-        set { WaterPoints = value; }
-    }
-
-    public float PointsMultiplier
-    { 
-        get { return PointsMultiplier; }
-        set { PointsMultiplier = value; }
-    }
-
-    public float Depth
-    {
-        get { return Depth; }
-        set { Depth = value; }
-    }
+    public int waterPoints = 0;
+    public float pointsMultiplier = 1;
+    public float depth = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        WaterPoints = 0;
-        PointsMultiplier = 1;
-        Depth = 0;
+        waterPoints = 0;
+        pointsMultiplier = 1;
+        depth = 0;
     }
 
     // Update is called once per frame
