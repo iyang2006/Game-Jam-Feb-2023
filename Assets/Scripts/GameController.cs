47d055af2d5055bba74ccf4e8d24a0c634ee8b57
williamhCode <83525937+williamhCode@users.noreply.github.com>
2023-02-04 02:46:38 -0500

    fix chunk gap

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,6 +21,8 @@ public class GameController : MonoBehaviour
         for (int i = DirtChunks.Count - 1; i >= 0; i--)
         {
             GameObject chunk = DirtChunks[i];
+            chunk.transform.Translate(Vector3.up * speed * Time.deltaTime);
+
             float y_pos = chunk.transform.position.y;
             if (y_pos > 7)
             {
@@ -31,11 +33,6 @@ public class GameController : MonoBehaviour
                 // Instantiate a new chunk
                 GameObject newChunk = Instantiate(DirtChunk, new Vector3(0, y_pos - 14, 0), Quaternion.identity);
                 DirtChunks.Add(newChunk);
-            }
-            else
-            {
-                // Move the chunk up
-                chunk.transform.Translate(Vector3.up * speed * Time.deltaTime);
             }
         }
 
