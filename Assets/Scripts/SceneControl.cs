524cb385577f3b807db018d97e77be5e20ebffc5
iyang2006 <yang2006@purdue.edu>
2023-02-04 13:58:58 -0500

    everythiing

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Logic : MonoBehaviour
+public class SceneControl : MonoBehaviour
 {
+    public GameObject gameOverScreen;
+
     // Button for start game (start menu -> game screen)
     public void startGame()
     {
         SceneManager.LoadScene("RunGameScene");
     }
 
+    // Logic for when game is over
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
     // Button for playing again (end menu -> game screen)
     public void playAgain()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Button for return to main menu (end menu -> start menu)
